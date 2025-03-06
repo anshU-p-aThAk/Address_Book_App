d424538b7468b0264e28402e20c749f01fd63bd3
Anshu Pathak <surajshiv15ss@gmail.com>
2025-03-06 19:50:31 +0530

    uc2 done

--- src/main/java/com/spring/addressBook/controller/AddressBookController.java
+++ src/main/java/com/spring/addressBook/controller/AddressBookController.java
@@ -1,8 +1,6 @@
 package com.spring.addressBook.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/address-book")
@@ -11,5 +9,30 @@ public class AddressBookController {
     @GetMapping("/api/test")
     public String test() {
         return "Hello from Address Book Controller";
+    }
+
+    @GetMapping({"", "/", "/get"})
+    public String getContact() {
+        return "Get Call Success";
+    }
+
+    @GetMapping("/get/{contactId}")
+    public String getContactById() {
+        return "Get Call Success for id";
+    }
+
+    @PostMapping("/create")
+    public String addContact() {
+        return "Create Call Success";
+    }
+
+    @PutMapping("/update/{contactId}")
+    public String updateContact() {
+        return "Update Call Success";
+    }
+
+    @DeleteMapping("/delete/{contactId}")
+    public String deleteContact() {
+        return "Delete Call Success";
     }
 }
