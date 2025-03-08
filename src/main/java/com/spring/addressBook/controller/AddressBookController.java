a13c23ed5794a5e2c422db90cfbc848ff5bcbad9
Anshu Pathak <surajshiv15ss@gmail.com>
2025-03-08 11:26:13 +0530

    uc3 done

--- src/main/java/com/spring/addressBook/controller/AddressBookController.java
+++ src/main/java/com/spring/addressBook/controller/AddressBookController.java
@@ -1,8 +1,10 @@
 package com.spring.addressBook.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.spring.addressBook.dto.AddressBookDTO;
+import com.spring.addressBook.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/address-book")
@@ -11,5 +13,32 @@ public class AddressBookController {
     @GetMapping("/api/test")
     public String test() {
         return "Hello from Address Book Controller";
+    }
+
+    @GetMapping({"", "/", "/get"})
+    public ResponseEntity<ResponseDTO> getContact() {
+        AddressBookDTO addressBook = new AddressBookDTO();
+        return new ResponseEntity<>(new ResponseDTO("Get Contacts", addressBook), HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{contactId}")
+    public ResponseEntity<ResponseDTO> getContactById() {
+        AddressBookDTO addressBook = new AddressBookDTO();
+        return new ResponseEntity<>(new ResponseDTO("Get Contact", addressBook), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO> addContact(@RequestBody AddressBookDTO addressBookDTO) {
+        return new ResponseEntity<>(new ResponseDTO("Add New Contact", addressBookDTO), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{contactId}")
+    public ResponseEntity<ResponseDTO> updateContact(@PathVariable Long contactId, @RequestBody AddressBookDTO addressBookDTO) {
+        return new ResponseEntity<>(new ResponseDTO("Update the Contact", addressBookDTO), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{contactId}")
+    public ResponseEntity<ResponseDTO> deleteContact(@PathVariable Long contactId) {
+        return new ResponseEntity<ResponseDTO>(new ResponseDTO("Deleted the contact with id: " + contactId, new AddressBookDTO()), HttpStatus.OK);
     }
 }
